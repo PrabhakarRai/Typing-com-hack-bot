9e2e66b39fbb93783163a8fad72b0c240c9ebc64
Prabhakar Rai <student343.bhu@gmail.com>
2020-06-14 23:47:23 +0530

    Create typingBot.user.js

---
+++ typingBot.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Typing.com Bot
+// @namespace    https://www.theprabhakar.in/
+// @version      1.0
+// @description  Browser counterpart of typing.com Bot
+// @author       Prabhakar Rai
+// @icon         https://raw.githubusercontent.com/PrabhakarRai/Typing-com-hack-bot/master/icon.png
+// @require https://code.jquery.com/jquery-3.5.1.min.js
+// ==/UserScript==
+
+//Will be release before 20/06/2020
